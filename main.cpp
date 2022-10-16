9b4d62a4cd8c4e2797774f49bc69bc8f0c6318fb
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2022-10-16 20:18:06 -0300

    First commit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,30 @@
-#include "io.hpp"
+#include <bits/stdc++.h>
+#include <fmt/core.h>
+#define ASIO_STANDALONE
+#include <asio.hpp>
+
+using namespace std;
+using namespace asio;
+using namespace asio::ip;
+
+static string get_line_from_client(tcp::socket& skt) {
+    string line;
+    read_until(skt, dynamic_buffer(line), '\n');
+    line.pop_back();
+    return line;
+}
+
+template <typename... T>
+static void put_to_client(tcp::socket& skt, string form, T... args) {
+    write(skt, buffer(fmt::format(fmt::runtime(form), args...)));
+}
+
+static void server(io_context& ctx, unsigned short port, function<void(tcp::socket)> fn) {
+    tcp::acceptor acpt(ctx, tcp::endpoint(tcp::v4(), port));
+    for (;;) {
+        std::thread(fn, acpt.accept()).detach();
+    }
+}
 
 mutex protect_global_data;
 set<string> user_names;
